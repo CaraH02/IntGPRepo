9bfb7abd4accfddeaf3f05c37c778da07b4cb96d
CaraH02 <carahenney@ymail.com>
2015-09-30 10:52:12 +0100

    test

--- UnityGitProject/Assets/TestScript.cs
+++ UnityGitProject/Assets/TestScript.cs
@@ -5,9 +5,9 @@ public class TestScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
+print("Hello World");
+}
+
 	// Update is called once per frame
 	void Update () {
 	
